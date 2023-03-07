8e704f16b2d78985375b178fb6d380fefd633f21
tarun.bhukya <tarun.bhukya92@gmail.com>
2023-03-07 19:55:32 +0530

    init

---
+++ components/homepage/ProgramCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full h-full rounded border-gray-100 border-2 p-4 text-white" v-bind:style="{ backgroundColor: color }">
+        <p class="text-center text-2xl">{{title}}</p>
+        <p class="mt-4 text-lg line-clamp-2">{{ content }}</p>
+        <a :href="url">View More</a>
+    </div>
+</template>
+
+<script setup>
+ defineProps({
+  color: String,
+  title: String,
+  content: String,
+  url: String,
+})
+</script>
+
